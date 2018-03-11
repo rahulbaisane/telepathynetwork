6dfe344481d54956144e357fa78715bdc1e67cd3
rahulbaisane <rahul_baisane@yahoo.com>
2018-03-11 09:07:02 +0530

    Time range module added

---
+++ modules/contrib/time_range/README.md
@@ -0,0 +1,28 @@
+Time Range
+==========
+Provides the form widget to fill in time range.  
+This is just a change to the display form, so that the user doesn't need to 
+enter too much content.  
+This module not stored time data. The storage of time is provided by the core.  
+
+## Installation
+
+Install the module as every other module.
+
+## Usage
+
+1. Enable the **Time Range** module.
+2. Create a **Date Range** field, select **Day and time** as field type.
+3. To select **Time range**, go to your content type's **Form Display Settings** 
+and select **Time Range**.
+
+Because it doesn't require you to fill out the date, the default record is the 
+date of the day.
+If you don't want to see the date on content's display, go to your content 
+type's display settings and change the 
+Date/Time format in your style.
+
+## Compatibility
+
+This module is compatible with Drupal core 8.x .
+
---
+++ modules/contrib/time_range/src/Plugin/Field/FieldWidget/TimeRangeWidget.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\time_range\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Plugin implementation of the 'time_range' widget.
+ *
+ * @FieldWidget(
+ *   id = "time_range",
+ *   label = @Translation("Time range"),
+ *   field_types = {
+ *     "daterange"
+ *   }
+ * )
+ */
+class TimeRangeWidget extends TimeRangeWidgetBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The date format storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $dateStorage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, EntityStorageInterface $date_storage) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
+
+    $this->dateStorage = $date_storage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['third_party_settings'],
+      $container->get('entity_type.manager')->getStorage('date_format')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+    $time_type = 'time';
+    $time_format = $this->dateStorage->load('html_time')->getPattern();
+
+    $element['value'] += [
+      '#date_date_format' => 'none',
+      '#date_date_element' => 'none',
+      '#date_date_callbacks' => [],
+      '#date_time_format' => $time_format,
+      '#date_time_element' => $time_type,
+      '#date_time_callbacks' => [],
+    ];
+
+    $element['end_value'] += [
+      '#date_date_format' => 'none',
+      '#date_date_element' => 'none',
+      '#date_date_callbacks' => [],
+      '#date_time_format' => $time_format,
+      '#date_time_element' => $time_type,
+      '#date_time_callbacks' => [],
+    ];
+
+    return $element;
+  }
+
+}
